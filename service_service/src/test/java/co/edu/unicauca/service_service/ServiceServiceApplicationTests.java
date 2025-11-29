c279d231d5f09c19270d6cfc8fbab5c1d1b1692e
Anvinov <anvigi2001@gmail.com>
2025-11-28 19:46:43 -0500

    Add services microservice

---
+++ service_service/src/test/java/co/edu/unicauca/service_service/ServiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.service_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
